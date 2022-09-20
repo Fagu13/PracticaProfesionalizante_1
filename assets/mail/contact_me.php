7dc56c4d75ef0dac2d7f659524e6dc5e33933157
Fede GU <fede.gu@hotmail.com>
2022-09-20 00:14:34 -0300

    ver1.1.1
    Se actualizó textos y parte de botones

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -1,5 +1,5 @@
 <?php
-// Revisa si los campos están vacíos
+// Revisar si los campos están vacíos
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
    empty($_POST['message'])	||
@@ -13,8 +13,8 @@ $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
-// Crea el mensaje que va a enviar
-$to = 'fede.gu.com'; // //Añada su dirección de correo electrónico reemplazando suNombre@suDominio.com - Aquí es donde el formulario enviará un mensaje. Puede ser @gmail.com
+// Crear el mensaje que va a enviar
+$to = 'fede.gu.com'; // //Aagrego su dirección de correo electrónico reemplazando suNombre@suDominio.com - Aquí es donde el formulario enviará un mensaje. Puede ser @gmail.com
 $email_subject = "formulario web enviado por:  $name";
 $email_body = "Ha recibido un nuevo mensaje de su formulario de contacto del sitio web.\n\n"."Aquí están los detalles:\n\nNombre: $name\n\nEmail: $email_address\n\n\Mensaje:\n$message";
 $headers = "From: noresponder@suDominio.com\n";// Esta es la dirección de correo electrónico de la que se generará el mensaje. DEBE TENER EL DOMINIO DONDE ESTÁ ALOJADA LA WEB
